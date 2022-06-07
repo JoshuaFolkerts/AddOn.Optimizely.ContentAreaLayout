27c011f76c5171c86f41f93766d4f84496f6db03
Erik Kärrsgård <erik.karrsgard@gmail.com>
2022-06-07 09:29:37 +0200

    Add icon to layout blocks to diffrentiate from regular ones

---
+++ Addon/Block/LayoutBlockUiDescriptor.cs
@@ -0,0 +1,13 @@
+﻿using EPiServer.Shell;
+
+namespace RenderingLayoutProcessor.Block
+{
+    [UIDescriptorRegistration]
+    public class LayoutBlockUiDescriptor : UIDescriptor<IRenderingLayoutBlock>
+    {
+        public LayoutBlockUiDescriptor()
+        {
+            IconClass = "epi-iconLayout";
+        }
+    }
+}
