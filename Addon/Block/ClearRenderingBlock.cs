7763ceac5c305af4b3845848f1c595b4814aa917
Wezz Balk <wezz.balk@epinova.se>
2022-09-08 13:37:28 +0200

    Hide block from being visible to editors. (Can be overriden in admin or inherited)

--- Addon/Block/ClearRenderingBlock.cs
+++ Addon/Block/ClearRenderingBlock.cs
@@ -3,7 +3,7 @@ using AddOn.Optimizely.ContentAreaLayout.Context;
 
 namespace AddOn.Optimizely.ContentAreaLayout.Block
 {
-    [ContentType(DisplayName = "Clear Rendering Layout", GroupName = "Layout", GUID = "b15b4a5b-2f99-47ae-aceb-c666eabaafd2", Description = "Clears multicolumn layout.")]
+    [ContentType(DisplayName = "Clear Rendering Layout", GroupName = "Layout", GUID = "b15b4a5b-2f99-47ae-aceb-c666eabaafd2", Description = "Clears multicolumn layout.", AvailableInEditMode = false)]
     public class ClearRenderingBlock : RenderingLayoutBlock
     {
         public override IRenderingContentAreaContext NewContext() =>
