cd5a390d6ff48969975e2debea713ffd54f81998
Wezz Balk <wezz.balk@epinova.se>
2024-03-04 09:17:59 +0100

    Enable inline blocks for demo  site

--- Site/Models/MultiCol/BootstrapLayoutBlock.cs
+++ Site/Models/MultiCol/BootstrapLayoutBlock.cs
@@ -2,12 +2,15 @@
 using AddOn.Optimizely.ContentAreaLayout;
 using AddOn.Optimizely.ContentAreaLayout.Block;
 using AddOn.Optimizely.ContentAreaLayout.Context;
+using System.ComponentModel.DataAnnotations;
 
 namespace Site.Models.MultiCol
 {
     [ContentType(DisplayName = "BootstrapLayoutBlock", GUID = "413b7a71-8490-4f22-bddc-77325c53424f", Description = "")]
     public class BootstrapLayoutBlock : RenderingLayoutBlock
     {
+        [Display(Name = "Text")]
+        public virtual string Text { get; set; }
         public override IRenderingContentAreaContext NewContext() =>
             new BootstrapRenderingContext(new[] { 2, 8, 2 });
     }
--- Site/Models/MultiCol/FoundationLayoutBlock.cs
+++ Site/Models/MultiCol/FoundationLayoutBlock.cs
@@ -2,12 +2,15 @@
 using AddOn.Optimizely.ContentAreaLayout;
 using AddOn.Optimizely.ContentAreaLayout.Block;
 using AddOn.Optimizely.ContentAreaLayout.Context;
+using System.ComponentModel.DataAnnotations;
 
 namespace Site.Models.MultiCol
 {
     [ContentType(DisplayName = "FoundationLayoutBlock", GUID = "51224645-4175-4462-AFEA-8787704381A3", Description = "")]
     public class FoundationLayoutBlock : RenderingLayoutBlock
     {
+        [Display(Name = "Text")]
+        public virtual string Text { get; set; }
         public override IRenderingContentAreaContext NewContext() =>
             new FoundationRenderingContext(new[] { 2, 8, 2 });
     }
